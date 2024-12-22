8402798c6b35674cecf72f0c39236384b7b1830b
Faysal2123 <foysal2005n1@gmail.com>
2024-12-22 21:34:02 +0600

    navbar section done

---
+++ src/components/MainLayouts/Layouts/Navbar.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from '../../../assets/logo/logo2 .png'
+
+
+const Navbar = () => {
+  const Links = (
+    <>
+      <li>
+      <NavLink to='/'>
+        Home
+      </NavLink>
+      </li>
+     <li >
+     <NavLink to='/availableCars'>
+        Available Cars
+      </NavLink>
+     </li>
+      <li>
+      <NavLink to='/addCar'>
+         Add Car
+      </NavLink>
+      </li>
+      <li>
+      <NavLink to='/myCars'>
+      My Cars
+      </NavLink>
+      </li>
+      <li>
+      <NavLink to='/myBookings'>
+      My Bookings 
+      </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="items-center ">
+      <div className="navbar bg-slate-200">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+             {Links}
+            </ul>
+          </div>
+         <div className="flex gap-2 items-center">
+            <img src={logo} className="md:h-20 md:w-20  h-12 w-12" alt="" />
+            <a className=" lg:text-4xl text-2xl font-bold"><span className="">Elite</span>Rides</a>
+         </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-base">
+            {Links}
+          </ul>
+        </div>
+        <div className="navbar-end gap-2">
+          <a className="btn btn-accent">Login</a>
+          <a className="btn btn-error">Register</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import MainLayouts from "../MainLayouts/MainLayouts";
 
 const router=createBrowserRouter([
     {
         path:'/',
-        element:<Home></Home>
+        element:<MainLayouts></MainLayouts>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
     }
 ])
 
